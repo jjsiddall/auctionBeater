69c55417e723f1070ba3993cb95295d537492c8c
Jake <jacobsiddall@gmail.com>
2013-12-29 21:55:39 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AuctionBeater::Application.config.secret_key_base = 'eb83e1b2df6961afaa22716e61214691e8090d53186f9cb39036a17255077ac8ab3ab341f58c193337c52b08a5dfa750016e4d91114310c697b1b764485d7018'
